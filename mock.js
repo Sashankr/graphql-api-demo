ec5cf626f47c64dc6ad6f3a186fca00266d400ab
Sashankr <sashankr97@gmail.com>
2022-09-28 23:13:40 +0530

    added type defs

---
+++ mock.js
@@ -0,0 +1,30 @@
+export const UserList = [
+  {
+    id: 1,
+    name: "John",
+    username: "john",
+    age: 20,
+    nationality: "Canada",
+  },
+  {
+    id: 2,
+    name: "Pedro",
+    username: "pedro",
+    age: 20,
+    nationality: "Brazil",
+  },
+  {
+    id: 3,
+    name: "Sarah",
+    username: "sarah",
+    age: 25,
+    nationality: "USA",
+  },
+  {
+    id: 4,
+    name: "Joe",
+    username: "joe",
+    age: 25,
+    nationality: "USA",
+  },
+];
